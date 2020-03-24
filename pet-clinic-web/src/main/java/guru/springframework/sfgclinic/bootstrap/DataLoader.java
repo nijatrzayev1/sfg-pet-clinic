898d2b1accb7ce8c039b4a540df522848b93d90c
Nijat Rzayev <nicat_r@yahoo.com>
2020-03-24 19:46:17 +0400

    Map auto ID added

--- pet-clinic-web/src/main/java/guru/springframework/sfgclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgclinic/bootstrap/DataLoader.java
@@ -22,26 +22,22 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 =  new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Nijat");
         owner1.setLastName("Rzayev");
         ownerService.save(owner1);
 
         Owner owner2 =  new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Uluxan");
         owner2.setLastName("Rzayev");
         ownerService.save(owner2);
 
         System.out.println("Owners loaded..........");
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Hamu1");
         vet1.setLastName("HamuSoyad");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Hamu2");
         vet2.setLastName("HamuSoyad2");
         vetService.save(vet2);
